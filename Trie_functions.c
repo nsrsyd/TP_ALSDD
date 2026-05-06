b0f966d9a3d73cb214847fce2f62cd7c73f40b54
nsrsyd <nsrsad123@gmail.com>
2026-05-06 22:26:23 +0100

    Refactor Trie implementation and related functions

--- Trie_functions.c
+++ Trie_functions.c
@@ -1,65 +1,211 @@
-#include"sets_operations.h"
-#include"Trie_machine_abstract.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
 
-typedef struct paragraph_node {
-    Trie* sentence;
-    paragraph_node* next;
-    paragraph_node* prev;
-    
-}paragraph_node;
+typedef struct Trie_node{
+    struct Trie_node* children[26];
+    bool is_end_of_word;
+}Trie_node;
 
-typedef struct paragraph
-{
-    paragraph_node* head;
-    paragraph_node* tail;
-}paragraph;
+typedef struct Trie{
+    Trie_node* Root;
+}Trie;
 
 
-void prep_paragraph_node(paragraph_node* node){
-    node->sentence = create_trie();
-    node->next = node->prev = NULL;
+Trie* create_trie(){
+    Trie* trie = malloc(sizeof(Trie));
+    trie->Root = create_trie_node();
+    return trie;
 }
-
-paragraph_node* creat_paragraph_node(){
-    paragraph_node* node =  malloc(sizeof(paragraph_node));
-    prep_paragraph_node(node);
+void prep_trie_node(Trie_node* node){
+    node->is_end_of_word = 0;
+    for (int i = 0 ; i<26 ; i++) {
+        node->children[i] = NULL;
+    }
+}
+Trie_node* create_trie_node(){
+    Trie_node* node = malloc(sizeof(Trie_node));
+    prep_trie_node(node);
     return node;
 }
-paragraph* creat_paragraph(){
-    paragraph* par = malloc(sizeof(paragraph));
-    par->head = par->tail = NULL;
-    return par;
+
+void insert(Trie* trie ,char* s){
+    if (!(trie->Root) || !(*s)) {
+        return ;
+    }
+    Trie_node* root = trie->Root;
+    Trie_node* curr = root;
+    int size = strlen(*s);
+    int index;
+    for (int i = 0 ; i<size ; i++){
+        index = s[i]-'a';
+        if (curr->children[index] == NULL){
+            Trie_node* node = create_trie_node();
+            curr->children[index] = node;
+        }
+        curr = curr->children[index];
+    }
+    curr->is_end_of_word = true;
+    return;
 }
 
-void fill_paragraph(paragraph* par , char* text , int s){
-    if (par == NULL || text == NULL){
+bool search (Trie* trie , char* s){
+    if (!(trie->Root) || !(*s)) {
+        return false;
+    }
+    Trie_node* root = trie->Root;
+    Trie_node* curr = root;
+    int size = strlen(*s);
+    int index;
+    for (int i = 0 ; i<size ; i++){
+        index = s[i]-'a';
+        if (curr->children[index] == NULL){
+            return false;
+        }
+        curr = curr->children[index];
+        }
+        if (curr->is_end_of_word == true){
+            return true;
+        }
+        return false;
+}
+
+bool has_children(Trie_node* node){
+    if (node == NULL){
+        return false;
+    }
+    for (int i = 0 ; i<26 ;i++){
+        if (node->children[i]!= NULL) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Trie_node* help_delete_word(Trie_node* root, char* s, int i) {
+    if (!root) return NULL;
+
+    if (s[i] == '\0') {
+        if (root->is_end_of_word) {
+            root->is_end_of_word = false;
+        }
+
+        if (!has_children(root)) {
+            free(root);
+            return NULL; 
+        }
+        return root;
+    }
+
+    int index = s[i] - 'a';
+    root->children[index] = help_delete_word(root->children[index], s, i + 1);
+
+    
+    if (!has_children(root) && !root->is_end_of_word) {
+        free(root);
+        return NULL;
+    }
+
+    return root;
+}
+
+void delete_word(Trie_node** root, char* s) {
+    *root = help_delete_word(*root, s, 0);
+}
+
+void help_free_trie(Trie_node* node){
+    if (node == NULL){
         return;
     }
-    char word[1000];
-    int j = 0;
-    if (par->head == NULL){
-    par->head = par->tail = creat_paragraph_node();
-}
-      paragraph_node* node = par->tail;
-    for(int i = 0 ; i<s ; i++){
-        if (text [i] == ' '){
-            word[j] = '\0';
-            insert(node->sentence , strdup(word));
-            j = 0;
-        }
-        else  if (text [i] == '.'){
-            word[j] = '\0';
-            insert(node->sentence , word);
-            paragraph_node* temp = creat_paragraph_node();
-            node->next = temp;
-            temp->prev = node;
-            node = node->next;
-            j = 0;
-        }
+    for (int i = 0 ; i<26 ; i++) {
+        if (node->children[i] != NULL) {
+            help_free_trie(node->children[i]);
+        }    }
+    free(node);
+    return;
 
-        else {
-            word[j] = text[i];
-            j++;
+}
+
+void free_Trie(Trie* trie){
+    if (trie == NULL || !trie->Root){
+        return;
+    }
+    help_free_trie(trie->Root);
+    free(trie);
+    return;
+}
+
+
+bool start_with (Trie* trie , char* s){
+    if (trie == NULL || trie->Root == NULL || s == NULL) {
+        return false;
+    }
+    Trie_node* curr = trie->Root;
+    int index ;
+    for (int i = 0 ; s[i] != '\0' ; i++)
+    {
+        index = s[i] - 'a';
+        if (curr->children[index] == NULL){
+            return false;
+        }
+        curr = curr->children[index];
+
+    }
+    return true;
+}
+
+
+void help_travers_most_common(Trie_node* node1 , char*word , int i , char** res ,int* k) {
+    if (node1 == NULL ){
+        return ;
+    }
+    word[i] = '\0';
+    if ((node1->is_end_of_word == true) ){
+        res[(*k)++] = strdup(word);
+        return;
+    }
+    for (int j = 0 ; j < 26 ; j++){
+        if ((node1->children[j] != NULL)){
+            word[i] = 'a'+j;
+            help_travers_most_common(node1->children[j] , word , i+1 , res , k);
         }
     }
+}
+
+char* most_common_roots(Trie* trie){
+    if (trie == NULL || trie->Root == NULL ) {
+        return NULL;
+    }
+    Trie_node* curr = trie->Root;
+    char* words [1000];
+    char word[100];
+    help_travers_most_common(curr , word , 0 , words , 0);
+}
+
+
+void help_print_words(Trie_node* root , char* word , int j){
+    if(root == NULL){
+        return;
+    }
+    word[j]='\0';
+    if(root->is_end_of_word){
+    printf(" %s " , word);
+    }
+    for (int i = 0 ; i < 26 ; i++){
+        if (root->children[i] != NULL){
+            word[j] = 'a'+i;
+            help_print_words(root->children[i] , word , j+1);
+        }
+    }
+
+}
+void print_words(Trie* trie) {
+    if(trie == NULL || trie->Root == NULL){
+        return;
+    }
+    char word[100];
+    printf("{ ");
+    help_print_words(trie->Root , word ,0);
+    printf(" }");
 }
