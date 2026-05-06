120fdf4155a693edc03b33c5336ecae013f49ebd
nsrsyd <nsrsad123@gmail.com>
2026-05-06 22:24:49 +0100

    Add paragraph data structure and related functions

--- Trie_functions.c
+++ Trie_functions.c
@@ -1,10 +1,65 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<stdbool.h>
+#include"sets_operations.h"
+#include"Trie_machine_abstract.h"
 
-typedef struct Trie_node{
-    char* arr[26];
-    int is_end_of_word;
+typedef struct paragraph_node {
+    Trie* sentence;
+    paragraph_node* next;
+    paragraph_node* prev;
+    
+}paragraph_node;
 
-}Trie_node;
+typedef struct paragraph
+{
+    paragraph_node* head;
+    paragraph_node* tail;
+}paragraph;
+
+
+void prep_paragraph_node(paragraph_node* node){
+    node->sentence = create_trie();
+    node->next = node->prev = NULL;
+}
+
+paragraph_node* creat_paragraph_node(){
+    paragraph_node* node =  malloc(sizeof(paragraph_node));
+    prep_paragraph_node(node);
+    return node;
+}
+paragraph* creat_paragraph(){
+    paragraph* par = malloc(sizeof(paragraph));
+    par->head = par->tail = NULL;
+    return par;
+}
+
+void fill_paragraph(paragraph* par , char* text , int s){
+    if (par == NULL || text == NULL){
+        return;
+    }
+    char word[1000];
+    int j = 0;
+    if (par->head == NULL){
+    par->head = par->tail = creat_paragraph_node();
+}
+      paragraph_node* node = par->tail;
+    for(int i = 0 ; i<s ; i++){
+        if (text [i] == ' '){
+            word[j] = '\0';
+            insert(node->sentence , strdup(word));
+            j = 0;
+        }
+        else  if (text [i] == '.'){
+            word[j] = '\0';
+            insert(node->sentence , word);
+            paragraph_node* temp = creat_paragraph_node();
+            node->next = temp;
+            temp->prev = node;
+            node = node->next;
+            j = 0;
+        }
+
+        else {
+            word[j] = text[i];
+            j++;
+        }
+    }
+}
